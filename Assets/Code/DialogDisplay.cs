1d6b4b3a53de5890cbbd398cfe5c94ac8aafcc80
Benni0072 <143964973+Benni0072@users.noreply.github.com>
2024-07-12 11:47:13 +0200

    Quests, Collider, Schrift

--- Assets/Code/DialogDisplay.cs
+++ Assets/Code/DialogDisplay.cs
@@ -4,8 +4,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.UI;
-using static UnityEditor.Progress;
+
 
 public class DialogDisplay : MonoBehaviour
 {
